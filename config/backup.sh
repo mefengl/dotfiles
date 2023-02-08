c48e03cde5f4a5193d245b4665458d145b5f8775
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-02-08 21:42:13 +0800

    Update config/backup.sh

--- config/backup.sh
+++ config/backup.sh
@@ -3,8 +3,8 @@
 file=$(ls ~/Downloads/twp-backup_* 2>/dev/null)
 
 if [ -n "$file" ]; then
-  mkdir -p ~/config/browser/twp
-  mv "$file" ~/config/browser/twp/twp-backup.txt
+  mkdir -p ~/config/twp
+  mv "$file" ~/config/twp/twp-backup.txt
 fi
 
 trash ~/Downloads/* 2>/dev/null
