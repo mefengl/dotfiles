6129f827ab859476ee25caf7092d041cfaf1158a
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-02-20 21:05:40 +0800

    Update config/backup.sh Update .config/yabai/yabairc

--- config/backup.sh
+++ config/backup.sh
@@ -7,4 +7,13 @@ if [ -n "$file" ]; then
   mv "$file" ~/config/twp/twp-backup.txt
 fi
 
+# my-ublock-static-filters_2023-02-20_15.53.49
+
+file=$(ls ~/Downloads/my-ublock-static-filters_* 2>/dev/null)
+
+if [ -n "$file" ]; then
+  mkdir -p ~/config/ublock
+  mv "$file" ~/config/ublock/my-ublock-static-filters.txt
+fi
+
 trash ~/Downloads/* 2>/dev/null
