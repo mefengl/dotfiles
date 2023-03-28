0d81747466a96da7d6b65172980b2e91c3dd64f3
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-28 17:34:17 +0800

    Update .config/global-npm-package.sh

--- dot_config/global-npm-package.sh
+++ dot_config/global-npm-package.sh
@@ -2,11 +2,14 @@
 
 # Define the packages to install
 packages=(
-  "pnpm"
-  "yarn"
-  "vercel"
-  "turbo"
-  "yo generator-code"
+"yarn"
+# vercel
+"vercel"
+"turbo"
+# vscode
+"yo generator-code"
+# github
+"@githubnext/github-copilot-cli"
 )
 
 # Define color codes for echoing messages
@@ -20,14 +23,25 @@ width=$(tput cols)
 # Calculate the width of the divider
 divider=$(printf "%-${width}s" "-" | tr ' ' '-')
 
-# Install each package and display a message
+# Install pnpm first
+echo -e "${GREEN}Installing pnpm...${NC}"
+npm install -g pnpm
+if [ $? -eq 0 ]; then
+  echo -e "${GREEN}Successfully installed pnpm.${NC}"
+else
+  echo -e "${RED}Failed to install pnpm.${NC}"
+  exit 1
+fi
+echo $divider
+
+# Install each package with pnpm and display a message
 for package in "${packages[@]}"; do
-  echo -e "${GREEN}Installing ${package}...${NC}"
-  npm install -g ${package}
+  echo -e "${GREEN}Installing ${package} with pnpm...${NC}"
+  pnpm install -g ${package}
   if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Successfully installed ${package}.${NC}"
+    echo -e "${GREEN}Successfully installed ${package} with pnpm.${NC}"
   else
-    echo -e "${RED}Failed to install ${package}.${NC}"
+    echo -e "${RED}Failed to install ${package} with pnpm.${NC}"
   fi
   echo $divider
 done
