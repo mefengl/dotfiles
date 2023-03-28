c3b5e2049ba02ac81c01c118a2cae9b5173a56ce
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-28 17:35:44 +0800

    Update .config/global-npm-package.sh

--- dot_config/global-npm-package.sh
+++ dot_config/global-npm-package.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Define the packages to install
-packages=(
-"yarn"
-# vercel
-"vercel"
-"turbo"
-# vscode
-"yo generator-code"
-# github
-"@githubnext/github-copilot-cli"
-)
-
 # Define color codes for echoing messages
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -23,25 +11,52 @@ width=$(tput cols)
 # Calculate the width of the divider
 divider=$(printf "%-${width}s" "-" | tr ' ' '-')
 
-# Install pnpm first
-echo -e "${GREEN}Installing pnpm...${NC}"
-npm install -g pnpm
+# Install pnpm using npm
+echo -e "${GREEN}Installing pnpm using npm...${NC}"
+npm -g install pnpm
 if [ $? -eq 0 ]; then
-  echo -e "${GREEN}Successfully installed pnpm.${NC}"
+  echo -e "${GREEN}Successfully installed pnpm using npm.${NC}"
 else
-  echo -e "${RED}Failed to install pnpm.${NC}"
+  echo -e "${RED}Failed to install pnpm using npm.${NC}"
   exit 1
 fi
 echo $divider
 
-# Install each package with pnpm and display a message
-for package in "${packages[@]}"; do
-  echo -e "${GREEN}Installing ${package} with pnpm...${NC}"
-  pnpm install -g ${package}
-  if [ $? -eq 0 ]; then
-    echo -e "${GREEN}Successfully installed ${package} with pnpm.${NC}"
-  else
-    echo -e "${RED}Failed to install ${package} with pnpm.${NC}"
-  fi
-  echo $divider
-done
+# Define the packages to install
+packages=(
+  "yarn"
+  "vercel"
+  "turbo"
+  "yo generator-code"
+)
+
+# Run pnpm setup command
+echo -e "${GREEN}Running pnpm setup command...${NC}"
+pnpm setup
+echo $divider
+
+# Check if the -u flag is present, uninstall packages if necessary
+if [ "$1" == "-u" ]; then
+  for package in "${packages[@]}"; do
+    echo -e "${GREEN}Uninstalling ${package} with pnpm...${NC}"
+    pnpm uninstall -g ${package}
+    if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Successfully uninstalled ${package} with pnpm.${NC}"
+    else
+      echo -e "${RED}Failed to uninstall ${package} with pnpm.${NC}"
+    fi
+    echo $divider
+  done
+else
+  # Install packages and display a message
+  for package in "${packages[@]}"; do
+    echo -e "${GREEN}Installing ${package} with pnpm...${NC}"
+    pnpm install -g ${package}
+    if [ $? -eq 0 ]; then
+      echo -e "${GREEN}Successfully installed ${package} with pnpm.${NC}"
+    else
+      echo -e "${RED}Failed to install ${package} with pnpm.${NC}"
+    fi
+    echo $divider
+  done
+fi
