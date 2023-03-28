f2979da955b5d1faa7eaf1040e2faf940938cb09
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-28 17:14:03 +0800

    Update .config/global-npm-package.sh

--- dot_config/global-npm-package.sh
+++ dot_config/global-npm-package.sh
@@ -14,6 +14,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Get the width of the terminal
+width=$(tput cols)
+
+# Calculate the width of the divider
+divider=$(printf "%-${width}s" "-" | tr ' ' '-')
+
 # Install each package and display a message
 for package in "${packages[@]}"; do
   echo -e "${GREEN}Installing ${package}...${NC}"
@@ -23,4 +29,5 @@ for package in "${packages[@]}"; do
   else
     echo -e "${RED}Failed to install ${package}.${NC}"
   fi
+  echo $divider
 done
