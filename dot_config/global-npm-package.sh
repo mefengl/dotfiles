34f9c5823ee1a251832bf808128d72320aa900aa
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-28 17:12:11 +0800

    Update .config/global-npm-package.sh

--- dot_config/global-npm-package.sh
+++ dot_config/global-npm-package.sh
@@ -1,8 +1,26 @@
-# package manager
-npm install -g pnpm
-npm install -g yarn
-# vercel
-npm install -g vercel
-npm install -g turbo
-# vscode
-npm install -g yo generator-code
+#!/bin/bash
+
+# Define the packages to install
+packages=(
+  "pnpm"
+  "yarn"
+  "vercel"
+  "turbo"
+  "yo generator-code"
+)
+
+# Define color codes for echoing messages
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Install each package and display a message
+for package in "${packages[@]}"; do
+  echo -e "${GREEN}Installing ${package}...${NC}"
+  npm install -g ${package}
+  if [ $? -eq 0 ]; then
+    echo -e "${GREEN}Successfully installed ${package}.${NC}"
+  else
+    echo -e "${RED}Failed to install ${package}.${NC}"
+  fi
+done
