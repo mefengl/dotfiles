e15fb09df69c2c597aa04dd538b39bd3365db3ca
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-04-07 21:54:37 +0800

    Update config/ghclone.sh Update .config/homebrew/Brewfile

--- config/ghclone.sh
+++ config/ghclone.sh
@@ -5,4 +5,3 @@ read -p "Enter GitHub username: " username
 mkdir -p ~/projects/$username; cd ~/projects/$username;
 gh repo list $username | awk '{print $1}' | xargs -L1 gh repo clone
 # find . -type d -name ".git" | xargs -P10 -L1 dirname | xargs -L1 -I {} sh -c "cd {}; gptcommit install"
-
