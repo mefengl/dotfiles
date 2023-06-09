1faf20334e4399a318bfe984690058f75004e285
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-06-09 19:00:02 +0800

    Update .config/pipm.sh

--- dot_config/pipm.sh
+++ dot_config/pipm.sh
@@ -2,7 +2,7 @@
 
 # Define the packages to install
 packages=(
-	"asitop"
+    "asitop"
 )
 
 # Define color codes for echoing messages
@@ -18,8 +18,8 @@ divider=$(printf "%-${width}s" "-" | tr ' ' '-')
 
 # Check if Python is installed
 if ! command -v python &>/dev/null; then
-	echo -e "${RED}Python is not installed. Please install Python before running this script.${NC}"
-	exit 1
+    echo -e "${RED}Python is not installed. Please install Python before running this script.${NC}"
+    exit 1
 fi
 
 echo $divider
@@ -27,33 +27,27 @@ echo $divider
 # Check if pip is installed
 pip_version=$(pip --version)
 if [[ -z "$pip_version" ]]; then
-	echo -e "${RED}pip is not installed. Please install pip before running this script.${NC}"
-	exit 1
+    echo -e "${RED}pip is not installed. Please install pip before running this script.${NC}"
+    exit 1
 fi
 
-# Check if the -f flag is present, force installation of all packages
-force=false
-if [ "$1" == "-f" ]; then
-	echo -e "${GREEN}Forcing installation of all packages.${NC}"
-	force=true
+# Upgrade pip
+echo -e "${GREEN}Upgrading pip...${NC}"
+pip install --upgrade pip
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Failed to upgrade pip.${NC}"
+    exit 1
 fi
+echo $divider
 
-# Install or uninstall packages based on the force flag and whether the package is installed
+# Install each package
 for package in "${packages[@]}"; do
-	if $force || ! pip list | grep -q $package; then
-		if $force; then
-			echo -e "${GREEN}Force installing ${package} with pip...${NC}"
-		else
-			echo -e "${GREEN}Installing ${package} with pip...${NC}"
-		fi
-		pip install ${package}
-		if [ $? -eq 0 ]; then
-			echo -e "${GREEN}Successfully installed ${package} with pip.${NC}"
-		else
-			echo -e "${RED}Failed to install ${package} with pip.${NC}"
-		fi
-	else
-		echo -e "${GREEN}${package} is already installed.${NC}"
-	fi
-	echo $divider
+    echo -e "${GREEN}Installing ${package} with pip...${NC}"
+    pip install ${package}
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}Successfully installed ${package} with pip.${NC}"
+    else
+        echo -e "${RED}Failed to install ${package} with pip.${NC}"
+    fi
+    echo $divider
 done
