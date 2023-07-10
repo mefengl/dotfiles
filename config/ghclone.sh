20914c699060fd5b56e490702fb3b97b591ca5ec
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-07-10 10:35:25 +0800

    Update config/ghclone.sh

--- config/ghclone.sh
+++ config/ghclone.sh
@@ -10,7 +10,7 @@ counter=0
 pids=()
 max_retries=3
 
-gh repo list $username | awk '{print $1}' | while read repo; do
+gh repo list $username --limit 100 | awk '{print $1}' | while read repo; do
 	retry_count=0
 	while [ $retry_count -lt $max_retries ]; do
 		gh repo clone "$repo" &
