0a2f8c2f046bb71c76ae6f28fc0dc1cd9ab15d66
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-07-10 10:40:48 +0800

    Update config/ghclone.sh

--- config/ghclone.sh
+++ config/ghclone.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-read -p "Enter GitHub username: " username
+if [ -z "$1" ]
+then
+  read -p "Enter GitHub username: " username
+else
+  username=$1
+fi
 
 mkdir -p ~/projects/$username
+rm -rf ~/projects/$username/*
 cd ~/projects/$username
 
 max_jobs=5
@@ -11,29 +17,29 @@ pids=()
 max_retries=3
 
 gh repo list $username --limit 100 | awk '{print $1}' | while read repo; do
-	retry_count=0
-	while [ $retry_count -lt $max_retries ]; do
-		gh repo clone "$repo" &
-		pid=$!
-		wait $pid
-		exit_status=$?
+  retry_count=0
+  while [ $retry_count -lt $max_retries ]; do
+    gh repo clone "$repo" &
+    pid=$!
+    wait $pid
+    exit_status=$?
 
-		if [ $exit_status -eq 0 ]; then
-			break
-		else
-			((retry_count++))
-			echo "Failed to clone $repo. Retrying ($retry_count/$max_retries)..."
-		fi
-	done &
+    if [ $exit_status -eq 0 ]; then
+      break
+    else
+      ((retry_count++))
+      echo "Failed to clone $repo. Retrying ($retry_count/$max_retries)..."
+    fi
+  done &
 
-	pids+=($!)
-	((counter++))
+  pids+=($!)
+  ((counter++))
 
-	if [ $counter -eq $max_jobs ]; then
-		wait "${pids[0]}"
-		pids=("${pids[@]:1}")
-		((counter--))
-	fi
+  if [ $counter -eq $max_jobs ]; then
+    wait "${pids[0]}"
+    pids=("${pids[@]:1}")
+    ((counter--))
+  fi
 done
 
 wait
