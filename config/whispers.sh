0ee7c03c4facae0a3c8ae023cc186e318d0f70f8
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-07-21 11:22:20 +0800

    Update config/whispers.sh

--- config/whispers.sh
+++ config/whispers.sh
@@ -2,14 +2,14 @@
 
 # Check if an argument was provided. If so, use that as input.
 if [ $# -eq 0 ]; then
-	echo "Please enter the string of URLs (press Ctrl-D when done):"
-	URLS_STRING=""
-	while IFS= read -r line; do
-		URLS_STRING+="$line "
-	done
+    echo "Please enter the string of URLs (press Ctrl-D when done):"
+    URLS_STRING=""
+    while IFS= read -r line; do
+        URLS_STRING+="$line "
+    done
 else
-	URLS_STRING="$1"
+    URLS_STRING="$1"
 fi
 
-# Extract URLs and pass them to whisper.sh using xargs
-echo "$URLS_STRING" | awk -v RS='[ ,]+' '/^https:\/\/www.youtube.com\// {print}' | xargs -P 0 -I{} bash ~/config/whisper.sh "{}"
+# Extract URLs, remove duplicates, and pass them to whisper.sh using xargs
+echo "$URLS_STRING" | awk -v RS='[ ,]+' '/^https:\/\/www.youtube.com\// {print}' | sort | uniq | xargs -P 0 -I{} bash ~/config/whisper.sh "{}"
