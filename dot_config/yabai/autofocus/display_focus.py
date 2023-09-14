a5993e573f90ddb4a43613ef568c05f8607dab35
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-09-14 23:31:42 +0800

    Remove .config/yabai/autofocus/display_focus.py Remove .config/yabai/autofocus/display_focus.txt

--- dot_config/yabai/autofocus/display_focus.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if sys.argv[1] == "write":
-    with open("display_focus.txt", "w") as f:
-        f.write(sys.argv[2])
-    
-if sys.argv[1] == "read":
-    with open("display_focus.txt", "r") as f:
-        print(f.readline())
