ffff39e070b8db29d8007b3c49e9f69710a75b91
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-09-19 22:03:54 +0800

    Update config/dalle2.sh Update .zshrc

--- config/dalle2.sh
+++ config/dalle2.sh
@@ -1,25 +1,72 @@
 #!/bin/bash
 
+# Function to process command-line arguments
+process_args() {
+  while [[ $# -gt 0 ]]
+  do
+    key="$1"
+    case $key in
+        -d|--description)
+        IMAGE_DESCRIPTION="$2"
+        shift
+        shift
+        ;;
+        -n|--num-images)
+        NUM_IMAGES="$2"
+        shift
+        shift
+        ;;
+        -s|--size)
+        IMAGE_SIZE="$2"
+        shift
+        shift
+        ;;
+        -p|--prefix)
+        PREFIX="$2"
+        shift
+        shift
+        ;;
+        *)
+        shift
+        ;;
+    esac
+  done
+}
+
+# Process command-line arguments
+process_args "$@"
+
 # Shuffle all API keys from the list
 API_KEYS_ARRAY=($(echo $OPENAI_API_KEYS | tr "," "\n"))
 RANDOM_KEY=$(shuf -e "${API_KEYS_ARRAY[@]}" | head -n 1)
 
-# Get user's input for image description
-read -p "Enter a description for the image you want to generate: " IMAGE_DESCRIPTION
+# If not provided, get user's input for image description
+if [[ -z "$IMAGE_DESCRIPTION" ]]; then
+  read -p "Enter a description for the image you want to generate: " IMAGE_DESCRIPTION
+fi
 
-# Get the number of images
-read -p "Enter the number of images you want (default is 1): " NUM_IMAGES
+# If a prefix is set, concatenate it with the description
+if [[ ! -z "$PREFIX" ]]; then
+  IMAGE_DESCRIPTION="$PREFIX, $IMAGE_DESCRIPTION"
+fi
+
+# If not provided, get the number of images
 if [[ -z "$NUM_IMAGES" ]]; then
+  read -p "Enter the number of images you want (default is 1): " NUM_IMAGES
+  if [[ -z "$NUM_IMAGES" ]]; then
     NUM_IMAGES=1
+  fi
 fi
 
-# Get the size of images
-read -p "Enter the size of the images (default is 1024x1024): " IMAGE_SIZE
+# If not provided, get the size of images
 if [[ -z "$IMAGE_SIZE" ]]; then
+  read -p "Enter the size of the images (default is 1024x1024): " IMAGE_SIZE
+  if [[ -z "$IMAGE_SIZE" ]]; then
     IMAGE_SIZE="1024x1024"
+  fi
 fi
 
-# Call OpenAI API to generate the images using the randomly chosen key
+# Call OpenAI API to generate the images
 RESPONSE=$(curl -s "https://api.openai.com/v1/images/generations" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $RANDOM_KEY" \
@@ -32,7 +79,7 @@ RESPONSE=$(curl -s "https://api.openai.com/v1/images/generations" \
 # Extract image URLs from the response
 IMAGE_URLS=($(echo "$RESPONSE" | jq -r '.data[].url'))
 
-# If no IMAGE_URLS, output the RESPONSE in red color
+# Output the response in red if no URLs
 if [[ -z "$IMAGE_URLS" ]]; then
     echo -e "\e[31m$RESPONSE\e[0m"
     exit 1
